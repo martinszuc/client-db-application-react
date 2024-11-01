7d8608e862483f8c034d93d463ba79e62de8bb0b
Martin Szuc <mszuc@redhat.com>
2024-11-01 12:20:10 +0100

    feat(firebase): admin setup

---
+++ src/scripts/setAdmin.ts
@@ -0,0 +1,31 @@
+const admin = require('firebase-admin');
+
+// Initialize Firebase Admin SDK using environment variables
+admin.initializeApp({
+    credential: admin.credential.cert({
+        type: process.env.FIREBASE_TYPE,
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        privateKeyId: process.env.FIREBASE_PRIVATE_KEY_ID,
+        privateKey: (process.env.FIREBASE_PRIVATE_KEY as string).replace(/\\n/g, '\n'),
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        clientId: process.env.FIREBASE_CLIENT_ID,
+        authUri: process.env.FIREBASE_AUTH_URI,
+        tokenUri: process.env.FIREBASE_TOKEN_URI,
+        authProviderX509CertUrl: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+        clientC509CertUrl: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+    }),
+});
+
+async function setAdminClaim() {
+    try {
+        const user = await admin.auth().getUserByEmail('matosuc@gmail.com');
+        await admin.auth().setCustomUserClaims(user.uid, { admin: true });
+        console.log(`Admin claim set for ${user.email}`);
+    } catch (error) {
+        console.error('Error setting admin claim:', error);
+    }
+}
+
+setAdminClaim();
+
+export{};
